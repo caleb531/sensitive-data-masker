fa1585c5ff4c913a801221fa494c8771f5dbe2da
Caleb Evans <caleb@calebevans.me>
2025-04-12 13:51:21 -0700

    Add logic to only run masking logic on allowed hosts
    Currently, the allowed host list is empty, so this extension is
    effectively rendered inert for the time being. The next step is to
    design the Options/Popup UI to set allowed hosts (and also add a context
    menu item for allowing the current page/host, as a convenience).

--- content-script.ts
+++ content-script.ts
@@ -38,15 +38,37 @@ const observer = new MutationObserver((mutations) => {
 	});
 });
 
-// Check the page for sensitive data when the DOM is first loaded (which is
-// guaranteed because the default runAt value for content scripts is
-// "document_idle", which guarantees the DOM is complete by the time the content
-// script runs; for more details, see the table in
-// <https://developer.chrome.com/docs/extensions/develop/concepts/content-scripts#run_time>)
-maskValuesInNodeTree(document.body);
-// Recheck page for sensitive data when DOM changes
-observer.observe(document.body, {
-	childList: true,
-	subtree: true,
-	characterData: true
-});
+// Return true if the given host string is among the list of allowed host
+// patterns; wildcards (e.g. *.example.com) are allowed
+function hostIsAllowed(currentHost: string, allowedHosts: string[]) {
+	return allowedHosts.some((host) => {
+		const escapedHost = host
+			// Escape special regex characters
+			.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+			// Replace wildcards (*) with the proper regex pattern to match any
+			// character except a dot
+			.replace(/\\\*/g, '[^\\.]*');
+		const regex = new RegExp(`^${escapedHost}$`);
+		return regex.test(currentHost);
+	});
+}
+
+async function main() {
+	const allowedHosts = (await chrome.storage.sync.get('allowed_hosts'))?.allowed_hosts ?? [];
+	if (!hostIsAllowed(window.location.host, allowedHosts)) {
+		return;
+	}
+	// Check the page for sensitive data when the DOM is first loaded (which is
+	// guaranteed because the default runAt value for content scripts is
+	// "document_idle", which guarantees the DOM is complete by the time the
+	// content script runs; for more details, see the table in
+	// <https://developer.chrome.com/docs/extensions/develop/concepts/content-scripts#run_time>)
+	maskValuesInNodeTree(document.body);
+	// Recheck page for sensitive data when DOM changes
+	observer.observe(document.body, {
+		childList: true,
+		subtree: true,
+		characterData: true
+	});
+}
+main();

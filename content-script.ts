1c8d49015f3a39f76a4a5c881e676b6920a15a7f
Caleb Evans <caleb@calebevans.me>
2025-04-12 09:49:06 -0700

    Refactor content script to be more robust

--- content-script.ts
+++ content-script.ts
@@ -1,34 +1,50 @@
+// Regular expressions representing sensitive data values, like currency values,
+// credit card numbers, etc.
 const patterns = {
-	currencyValue: /\s?((\d,?)+(\.\d{1,2})?(?:K|M|B|T)?)/gi
+	currencyValue: /(\$|€|£|¥)\s*((\d+,?)+(\.\d{1,2})?(?:K|M|B|T)?)/gi
 };
+
+// Recursively walk element and its descendants, and for any leaf node whose
+// textContent matches any of the designated patterns, mask the value
+function maskValuesInNodeTree(node: Node) {
+	if (
+		node.nodeType === Node.TEXT_NODE &&
+		node.textContent &&
+		// Do not process scripts or styles
+		node.parentElement?.nodeName !== 'SCRIPT' &&
+		node.parentElement?.nodeName !== 'STYLE'
+	) {
+		const match = node.textContent.match(patterns.currencyValue);
+		if (match) {
+			node.textContent = node.textContent.replace(patterns.currencyValue, ($0, $1) => {
+				return `${$1}x.xx`;
+			});
+		}
+	} else if (node.nodeType === Node.ELEMENT_NODE) {
+		// Recursively process elements to find text nodes
+		node.childNodes.forEach((childNode) => {
+			maskValuesInNodeTree(childNode);
+		});
+	}
+}
 
 const observer = new MutationObserver((mutations) => {
 	mutations.forEach((mutation) => {
-		// Recursively walk element and its children, and for any leaf node whose textContent matches the currencyValue pattern, replace it with '$x.xx'
-		const walk = (node: Node) => {
-			if (node.nodeType === Node.TEXT_NODE && node.textContent) {
-				const match = node.textContent.match(patterns.currencyValue);
-				if (match) {
-					console.log('mutation', node);
-					node.textContent = node.textContent.replace(patterns.currencyValue, 'x.xx');
-				}
-			} else if (node.nodeType === Node.ELEMENT_NODE) {
-				node.childNodes.forEach((childNode) => {
-					walk(childNode);
-				});
-			}
-		};
 		if (mutation.type === 'childList') {
 			mutation.addedNodes.forEach((node) => {
 				if (node.nodeType === Node.ELEMENT_NODE) {
-					walk(node);
+					maskValuesInNodeTree(node);
 				}
 			});
 		}
 	});
 });
 
-console.log(document.body);
+// Check the page for sensitive data when the DOM is first loaded
+document.addEventListener('DOMContentLoaded', () => {
+	maskValuesInNodeTree(document.body);
+});
+// Recheck page for sensitive data when DOM changes
 observer.observe(document.body, {
 	childList: true,
 	subtree: true,

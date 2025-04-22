337edf45ec01cf67321c7b1a99db137722b0104d
Caleb Evans <caleb@calebevans.me>
2025-04-22 15:40:34 -0700

    Remove extraneous space from Switch component

--- scripts/components/Switch.svelte
+++ scripts/components/Switch.svelte
@@ -5,7 +5,7 @@
 <label class="switch-container">
 	<!-- Make sure that the "switch-input" class is still applied even if other
 	classes are added via props -->
-	<input type	="checkbox" {...props} class:switch-input={true} />
+	<input type="checkbox" {...props} class:switch-input={true} />
 	<div class="switch-track">
 		<div class="switch-thumb"></div>
 	</div>

c738c8671d1bf9b3b9c45554ab488018a1c20eb3
Caleb Evans <caleb@calebevans.me>
2025-05-28 15:49:06 -0700

    Fix page not reloading if just added to whitelist

--- content-script.ts
+++ content-script.ts
@@ -137,11 +137,12 @@ async function main() {
 		subtree: true,
 		characterData: true
 	});
-	// Reload the page when instructed by the Options popup
-	chrome.runtime.onMessage.addListener((message) => {
-		if (message.type === 'reloadPage') {
-			window.location.reload();
-		}
-	});
 }
 main();
+
+// Reload the page when instructed by the Options popup
+chrome.runtime.onMessage.addListener((message) => {
+	if (message.type === 'reloadPage') {
+		window.location.reload();
+	}
+});
